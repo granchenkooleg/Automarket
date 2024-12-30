3cd3d5b44590f1b59838e5f08d928fe9e20d2a0e
Oleg <granchenkooleg@gmail.com>
2024-12-30 18:27:01 +0200

    Initial Commit

---
+++ Automarket/AutomarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomarketApp.swift
+//  Automarket
+//
+//  Created by Oleg Granchenko on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AutomarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
